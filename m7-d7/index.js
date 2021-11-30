667d5f26a1d3549472eeb53c400c0a1c16dc6f7a
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-11-30 19:10:02 +0100

    ts

--- m7-d7/index.js
+++ m7-d7/index.js
@@ -8,13 +8,15 @@ val4 = 'lol';
 let val5;
 val5 = 'lol';
 val5 = 5;
-// ****************************//
+const x = 4;
+// *************************************************************//
 let arr = [val1, val4];
 let arr2 = ['lol', 2, false, 2, 'lol'];
-let arr3 = ['lol', 2, false];
-// ****************************//
-let obj = {};
-// ****************************//
-let sum = (a, b, c) => a + b + c;
+let arr3 = ['lol', 2, false]; //tuple fixed length and fixed type
+// *************************************************************//
+let obj = { name: 'A', age: 1, education: { BS: 'b', MS: 'c' } };
+// *************************************************************//
+function sum(a, b, x) {
+    return a + b;
+}
 console.log(sum(5, 9));
-console.log(sum(5, 5, 9));
--- m7-d7/index.ts
+++ m7-d7/index.ts
@@ -12,18 +12,26 @@ type stringOrNum = string | number;
 let val5: stringOrNum;
 val5 = 'lol';
 val5 = 5;
-
-// ****************************//
+const x = 4;
+// *************************************************************//
 let arr: string[] = [val1, val4];
 
 let arr2: (string | number | boolean)[] = ['lol', 2, false, 2, 'lol'];
 
-let arr3: [string, number, boolean] = ['lol', 2, false];
+let arr3: [string, number, boolean] = ['lol', 2, false]; //tuple fixed length and fixed type
 
-// ****************************//
-let obj = {};
+// *************************************************************//
+let obj: {
+	name: string;
+	age: number;
+	education: {
+		BS: string;
+		MS: string;
+	};
+} = { name: 'A', age: 1, education: { BS: 'b', MS: 'c' } };
 
-// ****************************//
-let sum = (a: number, b: number, c?: number) => a + b + c;
+// *************************************************************//
+function sum(a: number, b: number, x?: number) {
+	return a + b;
+}
 console.log(sum(5, 9));
-console.log(sum(5, 5, 9));
